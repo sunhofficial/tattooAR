8e864532ee1d69d911d549c9f2d386aa21be64d9
Sunho <sunh803@naver.com>
2023-09-18 01:31:47 +0900

    feat: custom Slider

---
+++ tattoAR/tattoAR/Extensions/Color+.swift
@@ -0,0 +1,11 @@
+//
+//  Color+.swift
+//  tattoAR
+//
+//  Created by 235 on 2023/09/17.
+//
+
+import SwiftUI
+extension Color {
+    static let backGround = Color(UIColor(hexCode: "272828"))
+}
--- tattoAR/tattoAR/InputImageView.swift
+++ tattoAR/tattoAR/InputImageView.swift
@@ -8,44 +8,55 @@
 import SwiftUI
 import PhotosUI
 struct InputImageView: View {
-//    @State private var selectedItem: PhotosPickerItem?
-//    @ObservedObject var bgRemover = RemoveBackground()
     @ObservedObject var vm = InputImageViewModel()
     @State private var isFullScreenCoverPresented = false
+    @State private var sliderValue = 0.0
     var body: some View {
-        VStack {
-            PhotosPicker(selection: $vm.imageSelection, matching: .images) {
-                Rectangle()
-                    .fill(Color.gray)
-                    .opacity(0.3)
-                    .frame(maxHeight: UIScreen.main.bounds.height/2)
-                    .background(
-                        Group{
-                            if let objectImage = vm.objectImage {
-                                Image(uiImage: objectImage)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                            }else{
-                                Image(systemName: "photo.fill")
-                                    .resizable()
-                                .frame(width: 40,height: 40)}
-                        }
-                    )
-                    .padding(.horizontal,20)
-                    .padding(.top,10)
-                    .aspectRatio(1,contentMode: .fit)
+        ZStack{
+            Color.backGround
+            VStack {
+                PhotosPicker(selection: $vm.imageSelection, matching: .images) {
+                    Rectangle()
+                        .fill(Color.gray)
+                        .opacity(0.3)
+                        .frame(maxHeight: UIScreen.main.bounds.height/2)
+                        .aspectRatio(1,contentMode: .fit)
+                        .overlay(
+                            Group{
+                                if let objectImage = vm.objectImage {
+                                    Image(uiImage: objectImage)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                } else{
+                                    Image(systemName: "photo.fill")
+                                        .resizable()
+                                    .frame(width: 40,height: 40)}
+                            }
+                        )
+                }
+                Slider(value: $sliderValue, in: 0...10, step: 1)
+                    .tint(.black)
+                    .padding(.top,30)
+                    .overlay(GeometryReader{ geo in
+                        Text("\(sliderValue, specifier: "%.f")")
+                            .foregroundStyle(.black)
+                            .font(.system(size: 18, weight: .semibold))
+                            .alignmentGuide(HorizontalAlignment.leading){
+                                return $0[HorizontalAlignment.leading] - (geo.size.width - $0.width * 2) * sliderValue / 10  - 5
+                            }
+                            .frame(maxWidth: .infinity, alignment: .leading)
 
+                    }, alignment: .top)
             }
-            Spacer()
+            .padding()
         }
-        .padding()
-//        .onReceive(vm.$selectedImage, perform: { newImage in
-//                // selectedImage가 변경될 때 isFullScreenCoverPresented를 조절
-//                isFullScreenCoverPresented = newImage != nil
-//            })
-//        .fullScreenCover(isPresented: $isFullScreenCoverPresented, content: {
-//            ImageLiftView(selectImage: vm.selectedImage!)
-//        })
+        .ignoresSafeArea()
+    }
+}
+
+struct InputImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputImageView()
     }
 }
