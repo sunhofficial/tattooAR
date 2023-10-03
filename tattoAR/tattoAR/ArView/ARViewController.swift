14fce9684d9363a3686fbb623a1c40f7012605ec
Sunho <sunh803@naver.com>
2023-10-03 15:04:21 +0900

    feat: Detect Spot Image

--- tattoAR/tattoAR/ArView/ARViewController.swift
+++ tattoAR/tattoAR/ArView/ARViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreML
+import AVFoundation
 import ARKit
 enum HandPose {
     case rightRock
@@ -16,11 +17,15 @@ enum HandPose {
 final class ARViewController: UIViewController {
     var arScnView: ARSCNView!
     private var frameCounter: Int = 0 //매프레임마다 손모양 인식이 아니라 일정한 간격으로 수행하면 좀 더 부드러워짐
-    private let handPosePredictionInterval: Int = 30 //30frame마다
+    private let handPosePredictionInterval: Int = 100 //100frame마다
     var tatooImage: UIImage
     var blackPoint: Double = 0.0
-    private var handPose: HandPose = .background
+    //    private var handPose: HandPose = .background
+    private var tattoImageView: UIImageView
+    private var frameSize: Double = 0.0
     private var model = try? HandModel(configuration: MLModelConfiguration())
+    var x: Double = 0
+    var y: Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         arScnView = ARSCNView(frame: view.bounds)
@@ -28,20 +33,26 @@ final class ARViewController: UIViewController {
         arScnView.session.delegate = self
         let configuration = ARWorldTrackingConfiguration()
         arScnView.session.run(configuration)
+        arScnView.addSubview(tattoImageView)
+//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(saveImage))
+//        arScnView.addGestureRecognizer(tapGestureRecognizer)
 
     }
+//    @objc func saveImage() {
+//        UIImageWriteToSavedPhotosAlbum(arScnView.snapshot(), nil, nil, nil)
+//    }
     init(tatooImage: UIImage, blackPoint: Double) {
         self.tatooImage = tatooImage
         self.blackPoint = blackPoint
+        self.tattoImageView = UIImageView(image: tatooImage)
+        tattoImageView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
         super.init(nibName: nil, bundle: nil)
 
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-
 }
 
 extension ARViewController: ARSessionDelegate {
@@ -62,28 +73,55 @@ extension ARViewController: ARSessionDelegate {
         frameCounter += 1
         if frameCounter % handPosePredictionInterval == 0 {
             frameCounter = 0
-            makePrediction(handPoseObservation: handObservation)
+            let handPose = makePrediction(handPoseObservation: handObservation)
+            if handPose != .background {
+                let tattoPoint = findTatooSpot(handPoseObservation: handObservation)
+                tattoImageView.frame =  CGRect(x: tattoPoint.x, y: tattoPoint.y , width: frameSize, height: frameSize)
+            }
+            else {
+                tattoImageView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+            }
         }
     }
-    func makePrediction(handPoseObservation: VNHumanHandPoseObservation) {
+
+    func findTatooSpot(handPoseObservation: VNHumanHandPoseObservation) -> CGPoint {
+        guard let wristSpot = try? handPoseObservation.recognizedPoint(.wrist) else{ return CGPoint.zero}
+        guard let ringSpot = try? handPoseObservation.recognizedPoint(.ringMCP) else {return CGPoint.zero}
+        guard let middleSpot = try? handPoseObservation.recognizedPoint(.middleMCP) else {return CGPoint.zero}
+        let ringMiddleHalfSpotX = (ringSpot.location.x + middleSpot.location.x) / 2
+        let ringMiddleHalfSpotY = (ringSpot.location.y + middleSpot.location.y) / 2
+        let distanceY = wristSpot.location.y - ringMiddleHalfSpotY
+        let distanceX = wristSpot.location.x - ringMiddleHalfSpotX
+        let inclination = distanceY/distanceX
+        let distance = sqrt(pow(distanceX, 2) + pow(distanceY, 2))
+        let newX = sqrt(distance / (inclination * inclination + 1) ) + wristSpot.location.x
+        let newY = wristSpot.location.y + inclination * sqrt(distance / (inclination * inclination + 1))
+        frameSize = distanceX * 500
+        return CGPoint(x:newY * UIScreen.main.bounds.width, y: newX * UIScreen.main.bounds.height)
+    }
+
+    func makePrediction(handPoseObservation: VNHumanHandPoseObservation) -> HandPose {
         guard let keypointsMultiArray = try? handPoseObservation.keypointsMultiArray() else { fatalError()}
         do {
             let handPrediction = try model!.prediction(poses: keypointsMultiArray)
             let label = handPrediction.label
-            guard let confidence = handPrediction.labelProbabilities[label] else {return}
-            if confidence > 0.8 {
+            guard let confidence = handPrediction.labelProbabilities[label] else {return .background}
+            if confidence > 0.9 {
                 switch label {
                 case "rightRock":
-                    handPose = .rightRock
+                    return .rightRock
+                    //                    handPose = .rightRock
                 case "leftRock":
-                    handPose = .leftRock
+                    //                    handPose = .leftRock
+                    return .leftRock
                 default:
-                    handPose = .background
-                    break
+                    //                    handPose = .background
+                    return .background
                 }
             }
         } catch {
             print("PREDICTION ERRRRRROR")
         }
+        return .background
     }
 }
